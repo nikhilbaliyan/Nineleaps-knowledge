a0428753e0212ba5dbf995f1d3581ffebc4ec6bd
nikhilbaliyan <nikhilbaliyan113@gmail.com>
2019-03-04 13:27:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nineleapsknowledge/googlesignin/Categories.java
@@ -0,0 +1,70 @@
+package com.example.nineleapsknowledge.googlesignin;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.nineleapsknowledge.MainActivity;
+import com.example.nineleapsknowledge.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.net.URI;
+
+public class Categories extends AppCompatActivity {
+
+    GoogleSignInClient mGoogleSignInClient;
+    Button sign_out;
+    TextView nameTV;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+
+        sign_out = findViewById(R.id.log_out);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+
+        sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(Categories.this,"Successfully signed out",Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(Categories.this, MainActivity.class));
+                        finish();
+                    }
+                });
+    }
+}
